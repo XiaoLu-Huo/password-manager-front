a2c0fd49a0cd715660ab3e29c74cbd56dbc91b1e
xiaolu.huo <xiaolu.huo@thoughtworks.com>
2026-04-16 14:43:20 +0800

    init: Electron + React + TypeScript + Vite project scaffold

---
+++ src/main/main.ts
@@ -0,0 +1,2 @@
+// Electron Main Process entry point
+// Will be implemented in Task 1.7
---
+++ src/preload/preload.ts
@@ -0,0 +1,2 @@
+// Electron Preload Script - secure bridge via contextBridge
+// Will be implemented in Task 1.2
